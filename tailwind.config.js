dec62d1060228fb67c90335dd0e9638217f5fb72
Ain Catoni <aincatoni@Ains-MacBook-Pro.local>
2022-10-28 19:53:01 -0300

    crea repositorio

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      "white": "#ffffff",
+      "green-pcf": "#43d87E",
+      "yellow-pcf": "#e2ff3c",
+      "blue-pcf": "#2644f9",
+      "gray-pcf": "#F1F3F4",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
